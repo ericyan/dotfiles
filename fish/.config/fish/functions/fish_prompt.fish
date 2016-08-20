ef086eee815add4ca8a6541bd59d963b0c1bdfcc
Eric Yan <long@ericyan.me>
2016-08-20 11:58:39 +0800

    Show Git infomation in prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -2,6 +2,15 @@ function fish_prompt
   set -l cwd (set_color $fish_color_cwd) (pwd | sed "s:^$HOME:~:")
   set -l prompt (set_color $fish_color_comment) ' » '
 
-  echo -e -n -s '\n' $cwd $prompt
+  set -l git_branch (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  if [ $git_branch ]
+    if [ (command git status -s --ignore-submodules=dirty ^/dev/null) ]
+      set git_indicator (set_color d08770) '*' (set_color cyan)
+    end
+
+    set git_info (set_color cyan) ' (' $git_branch $git_indicator ')'
+  end
+
+  echo -e -n -s '\n' $cwd $git_info $prompt
   set_color normal
 end
